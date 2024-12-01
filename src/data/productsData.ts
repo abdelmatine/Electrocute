56f6759df616780f3e51016e268844444e3ed9a0
abdelmatine <abdelmatinesfar@gmail.com>
2024-12-01 18:26:00 +0100

    com

--- src/data/productsData.ts
+++ src/data/productsData.ts
@@ -7,12 +7,12 @@ import Img6 from "/assets/product/p-9.jpg";
 import Img7 from "/assets/product/p-7.jpg";
 
 export const products = [
-  { id: 1, img: Img1, title: "Boat Headphone", price: "120", aosDelay: "0" },
-  { id: 2, img: Img2, title: "Rocky Mountain", price: "420", aosDelay: "200" },
-  { id: 3, img: Img3, title: "Goggles", price: "320", aosDelay: "400" },
-  { id: 4, img: Img4, title: "Boat Headphone", price: "120", aosDelay: "0" },
-  { id: 5, img: Img5, title: "Boat Headphone", price: "120", aosDelay: "0" },
-  { id: 6, img: Img6, title: "Rocky Mountain", price: "420", aosDelay: "200" },
-  { id: 7, img: Img7, title: "Goggles", price: "320", aosDelay: "400" },
-  { id: 8, img: Img1, title: "Boat Headphone", price: "120", aosDelay: "0" },
-];
+    { id: 1, img: Img1, title: "Noise Cancelling Headphones", price: "120", aosDelay: "0" },
+    { id: 2, img: Img2, title: "Trail Running Shoes", price: "420", aosDelay: "200" },
+    { id: 3, img: Img3, title: "Polarized Sunglasses", price: "320", aosDelay: "400" },
+    { id: 4, img: Img4, title: "Wireless Earbuds", price: "150", aosDelay: "0" },
+    { id: 5, img: Img5, title: "Smartwatch", price: "180", aosDelay: "0" },
+    { id: 6, img: Img6, title: "Hiking Backpack", price: "450", aosDelay: "200" },
+    { id: 7, img: Img7, title: "Cycling Helmet", price: "350", aosDelay: "400" },
+    { id: 8, img: Img1, title: "Bluetooth Speaker", price: "110", aosDelay: "0" },
+  ];
